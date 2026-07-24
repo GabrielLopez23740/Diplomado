5ce9dd2e1913cfbc9be8db50aac3690d146cde4b
GabrielLopez23740 <mandujanotkm@gmail.com>
2026-07-23 18:49:20 -0600

    Cambio de Carpeta
    organizacion de codigo

---
+++ FrontEnd/lista-tareas-app/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <app-layout>
+    <h1>Bienvenido</h1>
+    <p>Aplicación de lista de tareas con Vue y una API en .NET.</p>
+  </app-layout>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
