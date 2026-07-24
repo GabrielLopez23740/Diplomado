239fc05be1a62e08ec50684363f9654ef205988a
GabrielLopez23740 <mandujanotkm@gmail.com>
2026-07-23 18:42:13 -0600

    Cambio de Rama Back
    cambie mi back  a la rama dev

---
+++ BackEnd/DiplomadoMelchor/DiplomadoMelchor/Migrations/20260723041736_Tareas.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiplomadoMelchor.Migrations
+{
+    public partial class Tareas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empleado",
+                columns: table => new
+                {
+                    IdEmpleado = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(90)", maxLength: 90, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    EstaActivo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleado", x => x.IdEmpleado);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Proyecto",
+                columns: table => new
+                {
+                    IdProyecto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    EstaActivo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proyecto", x => x.IdProyecto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarea",
+                columns: table => new
+                {
+                    IdTarea = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaCompromiso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EstadoTarea = table.Column<int>(type: "int", nullable: false),
+                    IdEmpleado = table.Column<int>(type: "int", nullable: false),
+                    IdProyecto = table.Column<int>(type: "int", nullable: false),
+                    EstaActivo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarea", x => x.IdTarea);
+                    table.ForeignKey(
+                        name: "FK_Tarea_Empleado_IdEmpleado",
+                        column: x => x.IdEmpleado,
+                        principalTable: "Empleado",
+                        principalColumn: "IdEmpleado",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tarea_Proyecto_IdProyecto",
+                        column: x => x.IdProyecto,
+                        principalTable: "Proyecto",
+                        principalColumn: "IdProyecto",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarea_IdEmpleado",
+                table: "Tarea",
+                column: "IdEmpleado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarea_IdProyecto",
+                table: "Tarea",
+                column: "IdProyecto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarea");
+
+            migrationBuilder.DropTable(
+                name: "Empleado");
+
+            migrationBuilder.DropTable(
+                name: "Proyecto");
+        }
+    }
+}
